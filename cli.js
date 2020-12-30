fa196a14421e2eed0facb12614ee5aa6dbb5f9e1
trickypr <trickypr@users.noreply.github.com>
2020-12-30 17:33:28 +1100

    Migrate to yargs

--- cli.js
+++ cli.js
@@ -1,75 +1,126 @@
 #!/usr/bin/env node
 
-const cli = require("cli");
-const GGF = require("./main.js");
+const yargs = require('yargs')
+const GGF = require('./main.js')
 
-let ggf = new GGF();
+let ggf = new GGF()
+
+const args = process.argv
 
 // Default Config
-let dc = ggf.getConfig();
+let dc = ggf.getConfig()
 
-cli.enable("help");
+yargs
+  .scriptName('get-google-fonts')
+  .command(
+    '$0',
+    'Download a font from google fonts',
+    (yargs) => {
+      yargs
+        .option('input', {
+          alias: 'i',
+          type: 'url',
+          describe: 'Input URL of CSS with fonts',
+        })
+        .option('output', {
+          alias: 'o',
+          type: 'string',
+          default: dc.outputDir,
+          description: 'Output directory',
+        })
+        .option('path', {
+          alias: 'p',
+          type: 'string',
+          default: dc.path,
+          desc: 'Path placed before every source of font in CSS',
+        })
+        .option('css', {
+          alias: 'c',
+          desc: 'Name of CSS file',
+          default: dc.cssFile,
+          type: 'string',
+        })
+        .option('template', {
+          alias: 't',
+          desc: 'Template of font filename',
+          type: 'string',
+          default: dc.template,
+        })
+        .option('useragent', {
+          alias: 'u',
+          desc: 'Template of font filename',
+          type: 'string',
+          default: dc.template,
+        })
+        .option('quiet', {
+          alias: 'q',
+          desc: "Don't displays a lot of useful information",
+          type: 'string',
+          default: false,
+        })
+        .option('base64', {
+          alias: 'b',
+          desc: 'Save fonts inside CSS file as base64 URIs',
+        })
+        .option('non-strict-ssl', {
+          desc:
+            'Force to accepts only valid SSL certificates; in some cases, ' +
+            'such proxy or self-signed certificates should be turned off',
+        })
+        .option('overwriting', {
+          alias: 'w',
+          desc: 'Allows overwrite existing files',
+        })
+        .option('print-options', {
+          desc: 'Shows result options object without performing any action',
+        })
+        .option('simulate', {
+          alias: 's',
+          desc: 'Simulation; No file will be saved',
+        })
+    },
+    (res) => {
+      const options = {
+        outputDir: res.output,
+        path: res.path,
+        cssFile: res.css,
+        userAgent: res.useragent || dc.userAgent,
+        verbose: !Boolean(res.quiet),
+        overwriting: Boolean(res.overwriting),
+        strictSSL: !Boolean(res['non-strict-ssl']),
+        base64: Boolean(res.base64),
+        simulate: Boolean(res.simulate),
+        template: res.template,
+      }
 
-let res = cli.parse({
-  input: ["i", "Input URL of CSS with fonts", "url"],
-  output: ["o", "Output directory", "string", dc.outputDir],
-  path: [
-    "p",
-    "Path placed before every source of font in CSS",
-    "string",
-    dc.path,
-  ],
-  css: ["c", "Name of CSS file", "string", dc.cssFile],
-  template: ["t", "Template of font filename", "string", dc.template],
-  useragent: ["u", "User-agent used at every connection", "string", false],
-  quiet: ["q", "Don't displays a lot of useful information"],
-  base64: ["b", "Save fonts inside CSS file as base64 URIs"],
-  "non-strict-ssl": [
-    false,
-    [
-      "Force to accepts only valid SSL certificates; in some cases,",
-      "such proxy or self-signed certificates should be turned off",
-    ].join(""),
-  ],
-  overwriting: ["w", "Allows overwrite existing files"],
-  "print-options": [
-    false,
-    "Shows result options object without performing any action",
-  ],
-  simulate: ["s", "Simulation; No file will be saved"],
-});
+      if (Boolean(res['print-options'])) {
+        console.log(options)
+        return
+      }
 
-const options = {
-  outputDir: res.output,
-  path: res.path,
-  cssFile: res.css,
-  userAgent: res.useragent || dc.userAgent,
-  verbose: !Boolean(res.quiet),
-  overwriting: Boolean(res.overwriting),
-  strictSSL: !Boolean(res["non-strict-ssl"]),
-  base64: Boolean(res.base64),
-  simulate: Boolean(res.simulate),
-  template: res.template,
-};
+      if (!res.input) {
+        console.error(
+          'Error: please provide a URL. Run --help for a list of options'
+        )
+        return
+      }
 
-if (Boolean(res["print-options"])) {
-  console.log(options);
-  return;
-}
-
-ggf
-  .download(res.input, {
-    outputDir: res.output,
-    path: res.path,
-    cssFile: res.css,
-    userAgent: res.useragent || dc.userAgent,
-    verbose: !Boolean(res.quiet),
-    overwriting: Boolean(res.overwriting),
-    strictSSL: !Boolean(res["non-strict-ssl"]),
-    base64: Boolean(res.base64),
-    simulate: Boolean(res.simulate),
-    template: res.template,
-  })
-  .catch((e) => {
-    console.error(e);
-  });
+      ggf
+        .download(res.input, {
+          outputDir: res.output,
+          path: res.path,
+          cssFile: res.css,
+          userAgent: res.useragent || dc.userAgent,
+          verbose: !Boolean(res.quiet),
+          overwriting: Boolean(res.overwriting),
+          strictSSL: !Boolean(res['non-strict-ssl']),
+          base64: Boolean(res.base64),
+          simulate: Boolean(res.simulate),
+          template: res.template,
+        })
+        .catch((e) => {
+          console.error(e)
+        })
+    }
+  )
+  .help().argv
